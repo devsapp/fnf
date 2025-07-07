aaa1954133028e76ee99659aa750354f8d9d4dee
rsonghuster <ls_huster@163.com>
2025-07-08 00:17:12 +0800

    fix logger when multi resources

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,26 @@ import GLogger from './common/logger';
 
 export default class ComponentFnf {
   protected commands: any;
+  private logger: any;
   constructor({ logger }: any) {
-    GLogger.setLogger(logger || console);
+    this.logger = logger;
     this.commands = COMMAND_HELP;
   }
 
   async deploy(inputs: any) {
+    GLogger.setLogger(this.logger);
     const flowObj = new FnfFlow(inputs);
     return await flowObj.deploy();
   }
 
   async remove(inputs: any) {
+    GLogger.setLogger(this.logger);
     const flowObj = new FnfFlow(inputs);
     return await flowObj.remove();
   }
 
   async info(inputs: any) {
+    GLogger.setLogger(this.logger);
     const flowObj = new FnfFlow(inputs);
     return await flowObj.info();
   }
@@ -29,6 +33,7 @@ export default class ComponentFnf {
   // }
 
   async list(inputs) {
+    GLogger.setLogger(this.logger);
     const flowObj = new FnfFlow(inputs);
     return await flowObj.list();
   }
